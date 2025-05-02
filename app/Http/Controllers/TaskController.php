c3f933c4c6e65c74433c1ba13697c7ba998fedf7
terry-man-com <terry.man.com@icloud.com>
2025-05-03 03:43:24 +0900

    add:タスク登録機能

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        return view ('tasks.index', compact('tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('tasks.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'contents' => 'required|array',
+            'contents.*' => 'nullable|string|max:25',
+        ]);
+        foreach ($request->input('contents') as $content) {
+            if (!empty($content)) {
+                Task::create(['contents' => $content]);
+            }
+        }
+        return redirect()->route('task.create')->with('success', 'タスクを登録しました！');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        //
+    }
+}
---
+++ resources/views/Tasks/create.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>タスク登録</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1:wght@100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <header class="header">
+        <h1 class="header-title">
+            <a href="{{route('task.index')}}">
+                My Tasks
+            </a>
+        </h1>
+    </header>
+    <main class="main-wrapper">
+        <h1 class="main-title">タスク登録</h1>
+        @if (session('success'))
+            <div class="message">{{ session('success' )}}</div>
+        @endif
+        <div class="form-wrapper">
+            <form class="task-form" method="post" action="{{ route('task.store') }}">
+                @csrf
+                <ul>
+                        <li>
+                            <input type="text" name="contents[]" placeholder="25文字以内で入れてください">
+                        </li>
+                    @for($i = 0; $i < 4; $i++)
+                        <li>
+                            <input type="text" name="contents[]" placeholder="">
+                        </li>
+                    @endfor
+                </ul>
+                <div class="button-area">
+                    <button type="submit" class="create-button">新規登録</button>
+                    <a class="button back-button" href="{{ route('task.index') }}">戻る</a>
+                </div>
+            </form>
+        </div>
+    </main>
+</body>
+</html>
